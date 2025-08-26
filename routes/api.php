c4cac54c4bd44e8ef913510738d9ede4d0041e5a
faiz832 <faqodefazz@gmail.com>
2025-08-27 05:44:30 +0700

    asd

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
 Route::get('/ping', fn () => response()->json(['message' => 'API is working']));
-
+Route::get('/ping', fn() => response()->json(['message' => 'pong']));
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login',    [AuthController::class, 'login']);
 Route::post('/logout',   [AuthController::class, 'logout']);
