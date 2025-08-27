2e6fe0271e73d4dbe97bb5adba867d894a2cb860
faiz832 <faqodefazz@gmail.com>
2025-08-27 19:17:06 +0700

    ddddd

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -10,7 +11,33 @@ Route::get('/login', function () {
     return view('login');
 });
 
-Route::get('/ping', fn () => response()->json(['message' => 'API is working!']));
+Route::get('/ping', fn () => response()->json(['message' => 'Pong from web.php!']));
+Route::get('/api', fn () => response()->json(['message' => 'API is working!']));
+Route::get('/api/v1/ping', fn () => response()->json(['message' => 'Pong from api.php!']));
+Route::get('/api/v1/user', function () {
+    $users = [
+        [
+            'id' => 1,
+            'name' => 'John Doe',
+            'email' => 'johndoe@example.com',
+            'role' => 'admin',
+        ],
+        [
+            'id' => 2,
+            'name' => 'Jane Smith',
+            'email' => 'janesmith@example.com',
+            'role' => 'user',
+        ],
+        [
+            'id' => 3,
+            'name' => 'Peter Jones',
+            'email' => 'peterjones@example.com',
+            'role' => 'user',
+        ],
+    ];
+
+    return response()->json($users);
+});
 
 // auth
 Route::post('/api/register', [AuthController::class, 'register']);
